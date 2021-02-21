76fedb7ec73fce7e3b4be3e8be78a75095bc661a
Jeremias Amestoy <jeremiasamestoy@gmail.com>
2021-02-21 01:14:57 -0300

    agregado estilo y reemplazado metodo write por textContent

--- orden3numeros.js
+++ orden3numeros.js
@@ -4,29 +4,40 @@ let c = parseInt(prompt("introduzca el tercer numero:"));
 
 //numbers.textContent = `Los numeros introducidos son ${a}, ${b}, ${c}`;
 
-document.write(` Los numeros ingresados son: a= ${a}, b= ${b}, c= ${c} <br>`);
+const numbers= document.getElementById('numbers');
+const results= document.getElementById('results');
+const palabra = document.getElementById('palabra');
+const conteo = document.getElementById('conteo');
+
+
+numbers.textContent = ` Primer numero= ${a}, Segundo numero= ${b}, Tercer numero= ${c}`;
+
+//document.write(` Los numeros ingresados son: a= ${a}, b= ${b}, c= ${c} <br>`);
 
 
 
 if (a>=b && a>=c){
   
-    if (b>c) {document.write(`Ordenados de mayor a menor: ${a}, ${b}, ${c}`);}
-    else {document.write(`Ordenados de mayor a menor: ${a}, ${c}, ${b}`);}
+    if (b>c) result.textContent =  `Ordenados de mayor a menor: ${a}, ${b}, ${c}`;
+    else result.textContent =  `Ordenados de mayor a menor: ${a}, ${c}, ${b}`;
 } 
 
 else if (b>a && b>c){
     
-    if (a>c) document.write(`Ordenados de mayor a menor: ${b}, ${a}, ${c}`);
-    else document.write(`Ordenados de mayor a menor: ${b}, ${c}, ${a}`);
+    if (a>c) result.textContent =  `Ordenados de mayor a menor: ${b}, ${a}, ${c}`;
+    else result.textContent =  `Ordenados de mayor a menor: ${b}, ${c}, ${ac}`;
 }
 
 else if (c>a && c>b){
     
-    if (a>b) document.write(`Ordenados de mayor a menor: ${c}, ${a}, ${b}`);
-    else document.write(`Ordenados de mayor a menor: ${c}, ${b}, ${a}`);
+    if (a>b) result.textContent =  `Ordenados de mayor a menor: ${c}, ${a}, ${b}`;
+    else result.textContent =  `Ordenados de mayor a menor: ${c}, ${b}, ${a}`;
         
 }
 
-let palabra = prompt(`Introduzca una palabra:`);
+let pala = prompt(`Introduzca una palabra:`);
+let longitud_pala = pala.length;
 
-alert(`La palabra ${palabra} contiene ${palabra.length} letras`);
+palabra.textContent = `${pala}`;
+conteo.textContent = `${longitud_pala} letras`;
+
